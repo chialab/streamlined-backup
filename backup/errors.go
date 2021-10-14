35046ec4dcd9394b4f4cbf30f6cf3b080583c8eb
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2021-10-14 10:19:06 +0200

    feat: add helper method to test for TaskError and its code

--- backup/errors.go
+++ backup/errors.go
@@ -1,6 +1,11 @@
 package backup
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+
+	"github.com/hashicorp/go-multierror"
+)
 
 type ErrorCode int
 
@@ -40,4 +45,40 @@ func (e TaskError) Error() string {
 
 func (e TaskError) Unwrap() error {
 	return e.previous
+}
+
+func IsTaskError(err error, code ...ErrorCode) bool {
+	if err == nil {
+		return false
+	}
+
+	check := func(taskErr *TaskError) bool {
+		if len(code) == 0 {
+			return true
+		}
+
+		for _, c := range code {
+			if taskErr.Code() == c {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	if taskErr, ok := err.(*TaskError); ok {
+		return check(taskErr)
+	} else if merr, ok := err.(*multierror.Error); ok {
+		for _, err := range merr.Errors {
+			if IsTaskError(err, code...) {
+				return true
+			}
+		}
+
+		return false
+	} else if taskErr := new(TaskError); errors.As(err, &taskErr) {
+		return check(taskErr)
+	}
+
+	return false
 }
--- backup/errors_test.go
+++ backup/errors_test.go
@@ -2,7 +2,10 @@ package backup
 
 import (
 	"errors"
+	"fmt"
 	"testing"
+
+	"github.com/hashicorp/go-multierror"
 )
 
 func TestTaskError(t *testing.T) {
@@ -51,6 +54,75 @@ func TestTaskError(t *testing.T) {
 				if !errors.As(err, &previous) {
 					t.Errorf("expected previous error %s, got %s", tc.previous, err.Unwrap())
 				}
+			}
+		})
+	}
+}
+
+func TestIsTaskError(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		expected bool
+		err      error
+		codes    []ErrorCode
+	}
+	testCases := map[string]testCase{
+		"nil": {
+			err:      nil,
+			expected: false,
+			codes:    []ErrorCode{},
+		},
+		"TaskError": {
+			err:      NewTaskError(CommandTimeoutError, "test error", nil),
+			expected: true,
+			codes:    []ErrorCode{CommandFailedError, CommandTimeoutError},
+		},
+		"TaskError_wrong_code": {
+			err:      NewTaskError(HandlerError, "test error", nil),
+			expected: false,
+			codes:    []ErrorCode{CommandFailedError, CommandTimeoutError},
+		},
+		"TaskError_no_code_check": {
+			err:      NewTaskError(HandlerError, "test error", nil),
+			expected: true,
+			codes:    []ErrorCode{},
+		},
+		"TaskError_wrapped": {
+			err:      fmt.Errorf("wrapping: %w", NewTaskError(CommandTimeoutError, "test error", nil)),
+			expected: true,
+			codes:    []ErrorCode{CommandTimeoutError},
+		},
+		"MultiError": {
+			err: &multierror.Error{
+				Errors: []error{
+					NewTaskError(CommandTimeoutError, "test error", nil),
+					NewTaskError(CommandFailedError, "test error", nil),
+				},
+			},
+			expected: true,
+			codes:    []ErrorCode{CommandFailedError},
+		},
+		"MultiError_no_TaskError": {
+			err: &multierror.Error{
+				Errors: []error{
+					errors.New("test error"),
+					errors.New("test error"),
+				},
+			},
+			expected: false,
+			codes:    []ErrorCode{},
+		},
+		"other": {
+			err:      errors.New("test error"),
+			expected: false,
+			codes:    []ErrorCode{},
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			if actual := IsTaskError(tc.err, tc.codes...); actual != tc.expected {
+				t.Errorf("expected %t, got %t", tc.expected, actual)
 			}
 		})
 	}
